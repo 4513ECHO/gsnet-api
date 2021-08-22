1ab6b55f3b828a1f81a708c04d312e3523cddd2b
4513ECHO <4513echo@gmail.com>
2021-08-22 14:00:14 +0000

    [fix]

--- app/app/api/screenshot.py
+++ app/app/api/screenshot.py
@@ -3,16 +3,21 @@ import os
 from fastapi import APIRouter
 from fastapi.responses import FileResponse
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
 
 IMAGE_PATH = os.path.join(os.path.dirname(__file__), "image/screenshot.png")
+CHROME_BIN = "/usr/bin/chromium-browser"
+CHROME_DRIVER = "/usr/lib/chromium-browser/chromedriver"
 
 router = APIRouter()
 
 def create_driver(url, image_path):
-    driver = webdriver.Remote(
-        desired_capabilities=DesiredCapabilities.FIREFOX.copy(),
-        command_executor="http://0.0.0.0:4444/wd/hub",
+    options = Options()
+    options.binary_location = CHROME_BIN
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(
+        CHROME_DRIVER,
+        options=Options,
     )
     driver.get(url)
     screenshot = driver.save_screenshot(image_path)
