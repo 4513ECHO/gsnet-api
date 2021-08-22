46909ef4c41f76837bb2a7b6adc918e82d9b9ec5
4513ECHO <4513echo@gmail.com>
2021-08-22 10:26:23 +0000

    [fix]

--- app/app/api/screenshot.py
+++ app/app/api/screenshot.py
@@ -5,31 +5,20 @@ from fastapi.responses import FileResponse
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-class ExtDriver(webdriver.Remote):
-    def __init__(self, url, image_path):
-        super().__init__(
-            desired_capabilities=DesiredCapabilities.FIREFOX.copy(),
-            command_executor="http://0.0.0.0:4444/wd/hub",
-        )
-        self.url = url
-        self.image_path = image_path
-
-    def screenshot(self):
-        super().get(self.url)
-        return super().save_screenshot(self.image_path)
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.close()
-
 IMAGE_PATH = os.path.join(os.path.dirname(__file__), "image/screenshot.png")
 
 router = APIRouter()
 
+def create_driver(url, image_path):
+    driver = webdriver.Remote(
+        desired_capabilities=DesiredCapabilities.FIREFOX.copy(),
+        command_executor="http://0.0.0.0:4444/wd/hub",
+    )
+    driver.get(url)
+    screenshot = driver.save_screenshot(image_path)
+    return screenshot
+
 @router.get("/screenshot/{address}", response_class=FileResponse)
-async def screenshot(address: str):
-    with ExtDriver(f"http://{address}/", IMAGE_PATH) as driver:
-        screenshot = driver.screenshot()
+def screenshot(address: str):
+    screenshot = create_driver(f"http://{address}/", IMAGE_PATH)
     return FileResponse(screenshot)
