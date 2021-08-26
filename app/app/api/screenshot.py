66c39e472825dfd626face457ded3441b43e9744
4513ECHO <4513echo@gmail.com>
2021-08-26 08:26:43 +0000

    [fix]

--- app/app/api/screenshot.py
+++ app/app/api/screenshot.py
@@ -4,20 +4,18 @@ from fastapi import APIRouter
 from fastapi.responses import FileResponse
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import chromedriver_binary
 
 IMAGE_PATH = os.path.join(os.path.dirname(__file__), "image/screenshot.png")
-CHROME_BIN = "/usr/bin/chromium-browser"
-CHROME_DRIVER = "/usr/lib/chromium-browser/chromedriver"
 
 router = APIRouter()
 
 def create_driver(url, image_path):
     options = Options()
-    options.binary_location = CHROME_BIN
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(
-        CHROME_DRIVER,
-        options=Options,
+        options=options
     )
     driver.get(url)
     screenshot = driver.save_screenshot(image_path)
