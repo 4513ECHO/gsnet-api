4fd91ae2da359b9251da366c8db20f612b7bb27f
4513ECHO <4513echo@gmail.com>
2021-08-31 16:08:18 +0000

    [fix]

--- app/app/api/screenshot.py
+++ app/app/api/screenshot.py
@@ -2,26 +2,37 @@ import os
 import datetime
 
 from fastapi import APIRouter
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, HTMLResponse, Response
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 router = APIRouter()
+html = "<html><head><meta property='og:image' content='{0}'></head></html>"
 
-def create_image(url):
+
+def create_image(url: str) -> str:
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(
-        "/usr/lib/chromium-browser/chromedriver",
-        options=options
-    )
+    driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", options=options)
     driver.get(url)
-    image = f"{datetime.datetime.now():%Y%m%d%H%M%S%f}.png"
-    driver.save_screenshot(f"/tmp/{image}")
-    return image
+    filepath = f"/tmp/{datetime.datetime.now():%Y%m%d%H%M%S%f}.png"
+    driver.save_screenshot(filepath)
+    return filepath
 
-@router.get("/screenshot/{address}", response_class=FileResponse)
-def screenshot(address: str):
-    image = create_image(f"http://{address}/")
-    return FileResponse(f"/tmp/{image}")
+
+def is_ipaddress(site: str) -> bool:
+    if site.startswith("10."):
+        return True
+    return False
+
+
+@router.get("/screenshot")
+def screenshot(site: str, ogp: bool = False):
+    if not is_ipaddress(site):
+        return Response(status_code=400)
+    image = create_image(f"http://{site}")
+    if ogp:
+        return HTMLResponse(html.format(image))
+    else:
+        return FileResponse(image)
