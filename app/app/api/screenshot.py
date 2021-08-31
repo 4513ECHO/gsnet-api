8a18d979afcf740962c2995f1c56762d8360a833
4513ECHO <4513echo@gmail.com>
2021-08-31 15:04:34 +0000

    [fix] 完成

--- app/app/api/screenshot.py
+++ app/app/api/screenshot.py
@@ -1,15 +1,14 @@
 import os
+import datetime
 
 from fastapi import APIRouter
 from fastapi.responses import FileResponse
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-IMAGE_PATH = os.path.join(os.path.dirname(__file__), "image/screenshot.png")
-
 router = APIRouter()
 
-def create_driver(url, image_path):
+def create_image(url):
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
@@ -18,9 +17,11 @@ def create_driver(url, image_path):
         options=options
     )
     driver.get(url)
-    screenshot = driver.save_screenshot(image_path)
+    image = f"{datetime.datetime.now():%Y%m%d%H%M%S%f}.png"
+    driver.save_screenshot(f"/tmp/{image}")
+    return image
 
 @router.get("/screenshot/{address}", response_class=FileResponse)
 def screenshot(address: str):
-    create_driver(f"http://{address}/", IMAGE_PATH)
-    return FileResponse(IMAGE_PATH)
+    image = create_image(f"http://{address}/")
+    return FileResponse(f"/tmp/{image}")
